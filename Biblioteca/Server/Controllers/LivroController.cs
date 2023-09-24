df09b8df349c9182e40805351beffe65fdf56bab
VirsoRaivoky <wilson_xavier@hotmail.com.br>
2023-09-24 18:06:36 -0300

    inicializando

---
+++ Biblioteca/Server/Controllers/LivroController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Biblioteca.Shared;
+
+namespace Biblioteca.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivroController : ControllerBase
+    {
+        public async Task<ActionResult<List<Livro>>> GetAllLivros()
+        {
+            var list = new List<Livro>
+            {
+                new Livro{ Id = 1, Title = "Memo", Author = "M", Code = 15481, Genre = "R", ISBN = 158486, Publisher = "Editora", Year = 1900 } 
+            };
+            return Ok(list);
+        }
+    }
+}
---
+++ Biblioteca/Shared/Livro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Shared
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public string Publisher { get; set; }
+        public int Year { get; set; }
+        public int Code { get; set; }
+        public int ISBN { get; set; }
+
+    }
+}
